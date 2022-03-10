db521d2dd31c9ed484ee6d88a498a49c11323f17
serish <serishd@gmail.com>
2022-03-10 13:25:25 +0545

    shop page with collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import{Route,Switch} from 'react-router-dom';
 
 
@@ -9,6 +10,7 @@ function App() {
   <div>
     <Switch>
       <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
     </Switch>
   </div>
   );
